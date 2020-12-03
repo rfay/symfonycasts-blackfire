90023ba0e6887d0126414e83bf9b6b28c929f3a9
Jérôme Vieilledent <jerome.vieilledent@blackfire.io>
2020-12-03 11:52:52 +0100

    Fixing inefficent count

--- src/Service/CommentHelper.php
+++ src/Service/CommentHelper.php
@@ -3,20 +3,17 @@
 namespace App\Service;
 
 use App\Entity\User;
+use App\Repository\CommentRepository;
 
 class CommentHelper
 {
+    public function __construct(private CommentRepository $commentRepository)
+    {
+    }
+
     public function countRecentCommentsForUser(User $user): int
     {
-        $comments = $user->getComments();
-        $commentCount = 0;
-        $recentDate = new \DateTimeImmutable('-3 months');
-        foreach ($comments as $comment) {
-            if ($comment->getCreatedAt() > $recentDate) {
-                $commentCount++;
-            }
-        }
-
-        return $commentCount;
+        return $this->commentRepository
+            ->countForUser($user, new \DateTimeImmutable('-3 months'));
     }
 }
